941a4983e6b6f174d5b6e210b115f9d6d6128ce0
OJailson17 <JaylsonO17@gmail.com>
2023-04-15 21:37:21 -0300

    feat: add the cat benefits section

---
+++ src/pages/Home/components/CatBenefits/index.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import { BsArrowRight } from 'react-icons/bs';
+
+export const CatBenefits = () => {
+	return (
+		<section className='w-[90%] mx-auto mt-16 flex flex-col items-center gap-16 lg:flex-row'>
+			<div className='mt-1'>
+				<div className='w-12 h-1  bg-primary-text-color rounded-full' />
+
+				<h2 className='text-4xl font-bold text-primary-text-color mt-4'>
+					Why should you have a cat?
+				</h2>
+
+				<p className='mt-10 text-lg  text-primary-text-color font-medium'>
+					Having a cat around you can actually trigger the release of calming
+					chemicals in your body which lower your stress and anxiety leves
+				</p>
+
+				<div className='flex items-center gap-3 mt-6'>
+					<p className='text-xs font-bold text-[#29150799] cursor-pointer'>
+						READ MORE
+					</p>
+					<BsArrowRight color='#29150799' />
+				</div>
+			</div>
+
+			<div className='grid grid-cols-2 gap-4'>
+				<div className='flex flex-col gap-4 items-center'>
+					<img src='/assets/image-1.png' alt='' />
+					<img src='/assets/image-2.png' alt='' className='w-[75%] self-end' />
+				</div>
+				<img src='/assets/image-3.png' alt='' />
+			</div>
+		</section>
+	);
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/Header';
 import React from 'react';
 import { Discover } from './components/Discover';
+import { CatBenefits } from './components/CatBenefits';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
 			<Discover />
+			<CatBenefits />
 		</>
 	);
 };
